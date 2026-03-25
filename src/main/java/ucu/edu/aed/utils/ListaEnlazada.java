ce644980c711ad99f840ebdf2d30b8e283e7e31b
camiladacol <camila.dacol@correo.ucu.edu.uy>
2026-03-25 19:06:40 -0300

    Método obtener listo

--- src/main/java/ucu/edu/aed/utils/ListaEnlazada.java
+++ src/main/java/ucu/edu/aed/utils/ListaEnlazada.java
@@ -2,12 +2,57 @@ package ucu.edu.aed.utils;
 
 import ucu.edu.aed.tda.TDALista;
 
+import java.util.Comparator;
+import java.util.function.Predicate;
+
 public class ListaEnlazada <T> implements TDALista<T> {
     private Nodo<T> primero;
 
     @Override
-    public void agregar(T elem) {
+    public T obtener(int index) {
+        Nodo<T> actual = primero; //Se arranca del primer nodo de la lista
+        int contador = 0;
+        if (index < 0) //Se valida el indice para que no sea negativo
+        {
+            throw new IndexOutOfBoundsException("El indice se encuentra por fuera");
+        }
+        while (actual != null) //Se recorre la lista, nodo por nodo
+        {
+            if (contador == index) //Se llega a la posición buscada
+            {
+                return actual.getDato(); //Devuelve el dato del nodo actual
+            }
+            actual = actual.getSiguiente(); //Se avanza al siguiente nodo
+            contador++; //Aumenta el contador
+        }
 
-
+        return null;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
