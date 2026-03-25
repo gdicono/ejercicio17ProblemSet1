a60053ebf7170c842dae00cd68b6d40058e0dd27
Gonzalo Di Cono <gonzalo.dicono@correo.ucu.edu.uy>
2026-03-25 20:40:56 -0300

    Cambios en biblioteca

--- src/main/java/ucu/edu/aed/utils/Biblioteca.java
+++ src/main/java/ucu/edu/aed/utils/Biblioteca.java
@@ -2,28 +2,7 @@ package ucu.edu.aed.utils;
 
 public class Biblioteca {
 
-    private ListaEnlazada<Libro> libros;
 
-    public Biblioteca() {
-        this.libros = new ListaEnlazada<>();
-    }
 
-    public void agregarLibro(Libro libro) {
-        libros.agregar(libro);
-    }
-
-    public void agergarLibroExistente(Libro libro) {
-        if (//poner metodo aca para buscar(libro.getIsbn()) != null) { // si el libro ya existe en la biblioteca
-         //poner metodo aca para buscar(libro.getIsbn()).agregarStock(libro.getStock()); // agregamos el stock del libro existente con el nuevo libro
-        }
-        else {
-            agregarLibro(//poner metodo aca para buscar); // si el libro no existe lo agregamos normalmente
-        }
-    }
-
-    public ListaEnlazada<Libro> verLibrosDisponibles() {
-        return libros;
-    }
 
 }
-
